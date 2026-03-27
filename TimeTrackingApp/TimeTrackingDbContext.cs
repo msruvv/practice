5cb148a61d0ae19fddb94bce0d87fa7a8fa15f5f
msruvv <st.gr.815@gmail.com>
2026-03-27 13:58:46 +0300

    создал DbContext

---
+++ TimeTrackingApp/TimeTrackingDbContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TimeTrackingApp.Entities;
+
+namespace TimeTrackingApp
+{
+    public class TimeTrackingDbContext : DbContext
+    {
+        public TimeTrackingDbContext(DbContextOptions<TimeTrackingDbContext> options) : base(options)
+        {    
+        }
+
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Entities.Task> Tasks { get; set; }
+        public DbSet<TimeEntry> TimeEntries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            
+        }
+    }
+}
