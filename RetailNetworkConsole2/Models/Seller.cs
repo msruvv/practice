51098c27fbd9330bb0d68d033fb5a809dd709e1a
msruvv <st.gr.815@gmail.com>
2026-04-01 23:16:50 +0300

    добавлена валидация для продавца

--- RetailNetworkConsole2/Models/Seller.cs
+++ RetailNetworkConsole2/Models/Seller.cs
@@ -8,16 +8,41 @@ namespace RetailNetworkConsole2.Models
 {
     internal class Seller
     {
+        private DateTime _birthDate;
+
         public int SellerID { get; set; }
         public string LastName { get; set; }
         public string FirstName { get; set; }
         public string? Patronymic { get; set; }
-        public DateTime BirthDate { get; set; }
+
+        public DateTime BirthDate
+        {
+            get => _birthDate;
+            set
+            {
+                ValidateBirthDate(value);
+                _birthDate = value;
+            }
+        }
+
         public bool IsActive { get; set; }
 
         public ContactInfo? ContactInfo { get; set; }
         public ICollection<ShopSeller> ShopSellers { get; set; } = new List<ShopSeller>();
         public ICollection<Receipt> Receipts { get; set; } = new List<Receipt>();
+
+        private void ValidateBirthDate(DateTime birthDate)
+        {
+            if (birthDate > DateTime.Now)
+            {
+                throw new ArgumentException("Дата рождения не может быть в будущем");
+            }
+
+            if (birthDate.Year < 1900)
+            {
+                throw new ArgumentException("Год рождения не может быть ранее 1900");
+            }
+        }
 
         public override string ToString()
         {
